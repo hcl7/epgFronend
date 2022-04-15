4c9cdc2081171c03bf9796c7afeb8ac62beac8f0
Elvin Mucaj <emuco7@yahoo.com>
2022-04-15 20:21:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Head from './Components/Head.jsx';
 import './App.css';
+import { connect } from 'react-redux';
+import * as actions from './store/actions';
+import Home from './Components/Home';
+import EventDetails from './Components/EventDetails';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className='countainer'>
+          <Head />
+        </div>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/details/:name' component={EventDetails} />
+          </Switch>
+      </Router>
+    );
+  } 
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    //onAuthCheckState: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, combineReducers} from 'redux';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './store/reducer';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const logger = store => {
+    return next => {
+        return action =>{
+            console.log('[Midleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Midleware] next State', store.getState());
+            return result;
+        }
+    }
+}
+
+const rootReducer = combineReducers({
+    reducer: reducer
+});
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render( app, document.getElementById( 'root' ) );
 reportWebVitals();
